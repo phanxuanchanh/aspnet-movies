62740b8efab3695e62e95701ee46142e2c076daa
phanxuanchanh <contact@phanxuanchanh.com>
2025-08-10 14:22:15 +0700

    Update to new MSSQL.dll; remove unnecessary thread-safety mechanisms in PageVisitor.cs; streamline and clean up DBContext.cs

--- Src0Web/Common/PageVisitor.cs
+++ Src0Web/Common/PageVisitor.cs
@@ -1,22 +1,18 @@
 ﻿
-using System.Threading;
-
 namespace Web.Shared
 {
     public static class PageVisitor
     {
-        private static long _views = 0;
-
-        public static long Views => Interlocked.Read(ref _views);
+        public static long Views;
 
         public static void Add()
         {
-            Interlocked.Increment(ref _views);
+            Views ++;
         }
 
         public static void Remove()
         {
-            Interlocked.Decrement(ref _views);
+            Views --;
         }
     }
 }
--- Src0Web/Data/Models/DBContext.cs
+++ Src0Web/Data/Models/DBContext.cs
@@ -5,76 +5,18 @@ namespace Data.DAL
 {
     public class DBContext : SqlContext
     {
-        private bool disposed;
-        private SqlAccess<AppSetting> appSettings;
-        private SqlAccess<Role> roles;
-        private SqlAccess<User> users;
-        private SqlAccess<PaymentMethod> paymentMethods;
-        private SqlAccess<PaymentInfo> paymentInfos;
-        private SqlAccess<Film> films;
-        private SqlAccess<UserReaction> userReactions;
-        private SqlAccess<FilmMetadata> filmMetadata;
-        private SqlAccess<FilmMetaLink> filmMetaLinks;
-        private SqlAccess<People> people;
-        private SqlAccess<PeopleLink> peopleLinks;
-        private SqlAccess<Taxonomy> taxonomies;
-        private SqlAccess<TaxonomyLink> taxonomyLinks;
-
-        public DBContext()
-            : base()
-        {
-            appSettings = null;
-            roles = null;
-            users = null;
-            paymentInfos = null;
-            paymentMethods = null;
-            films = null;
-            userReactions = null;
-            filmMetadata = null;
-            filmMetaLinks = null;
-            people = null;
-            peopleLinks = null;
-            taxonomies = null;
-            taxonomyLinks = null;
-            disposed = false;
-        }
-
-        public SqlAccess<AppSetting> AppSettings { get { return InitSqlAccess<AppSetting>(ref appSettings); } }
-        public SqlAccess<Role> Roles { get { return InitSqlAccess<Role>(ref roles); } }
-        public SqlAccess<User> Users { get { return InitSqlAccess<User>(ref users); } }
-        public SqlAccess<PaymentMethod> PaymentMethods { get { return InitSqlAccess<PaymentMethod>(ref paymentMethods); } }
-        public SqlAccess<PaymentInfo> PaymentInfos { get { return InitSqlAccess<PaymentInfo>(ref paymentInfos); } }
-        public SqlAccess<Film> Films { get { return InitSqlAccess<Film>(ref films); } }
-        public SqlAccess<UserReaction> UserReactions { get { return InitSqlAccess<UserReaction>(ref userReactions); } }
-        public SqlAccess<FilmMetadata> FilmMetadata { get { return InitSqlAccess<FilmMetadata>(ref filmMetadata); } }
-        public SqlAccess<FilmMetaLink> FilmMetaLinks { get { return InitSqlAccess<FilmMetaLink>(ref filmMetaLinks); } }
-        public SqlAccess<People> People { get { return InitSqlAccess<People>(ref people); } }
-        public SqlAccess<PeopleLink> PeopleLinks { get { return InitSqlAccess<PeopleLink>(ref peopleLinks); } }
-        public SqlAccess<Taxonomy> Taxonomies { get { return InitSqlAccess<Taxonomy>(ref taxonomies); } }
-        public SqlAccess<TaxonomyLink> TaxonomyLinks { get { return InitSqlAccess<TaxonomyLink>(ref taxonomyLinks); } }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposed) return;
-
-            if (disposing)
-            {
-                DisposeSqlAccess(ref roles);
-                DisposeSqlAccess(ref users);
-                DisposeSqlAccess(ref paymentMethods);
-                DisposeSqlAccess(ref paymentInfos);
-                DisposeSqlAccess(ref films);
-                DisposeSqlAccess(ref userReactions);
-                DisposeSqlAccess(ref filmMetadata);
-                DisposeSqlAccess(ref filmMetaLinks);
-                DisposeSqlAccess(ref people);
-                DisposeSqlAccess(ref peopleLinks);
-                DisposeSqlAccess(ref taxonomies);
-                DisposeSqlAccess(ref taxonomyLinks);
-            }
-
-            disposed = true;
-            base.Dispose(disposing);
-        }
+        public SqlAccess<AppSetting> AppSettings => InitSqlAccess<AppSetting>();
+        public SqlAccess<Role> Roles => InitSqlAccess<Role>();
+        public SqlAccess<User> Users => InitSqlAccess<User>();
+        public SqlAccess<PaymentMethod> PaymentMethods => InitSqlAccess<PaymentMethod>();
+        public SqlAccess<PaymentInfo> PaymentInfos => InitSqlAccess<PaymentInfo>();
+        public SqlAccess<Film> Films => InitSqlAccess<Film>();
+        public SqlAccess<UserReaction> UserReactions => InitSqlAccess<UserReaction>();
+        public SqlAccess<FilmMetadata> FilmMetadata => InitSqlAccess<FilmMetadata>();
+        public SqlAccess<FilmMetaLink> FilmMetaLinks => InitSqlAccess<FilmMetaLink>();
+        public SqlAccess<People> People => InitSqlAccess<People>();
+        public SqlAccess<PeopleLink> PeopleLinks => InitSqlAccess<PeopleLink>();
+        public SqlAccess<Taxonomy> Taxonomies => InitSqlAccess<Taxonomy>();
+        public SqlAccess<TaxonomyLink> TaxonomyLinks => InitSqlAccess<TaxonomyLink>();
     }
 }
