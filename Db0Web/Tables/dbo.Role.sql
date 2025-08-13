9fc33afa367045b66c0cd05a33f741431d0b082c
phanxuanchanh <contact@phanxuanchanh.com>
2025-08-13 10:29:33 +0700

    Modify table: Role

--- Db0Web/Tables/dbo.Role.sql
+++ Db0Web/Tables/dbo.Role.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Role] (
   [ID] [varchar](100) NOT NULL,
   [name] [nvarchar](50) NOT NULL,
-  [createAt] [datetime] NULL,
-  [updateAt] [datetime] NULL,
+  [CreatedAt] [datetime] NULL,
+  [UpdatedAt] [datetime] NULL,
+  [DeletedAt] [varchar](50) NULL,
   CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED ([ID])
 )
 ON [PRIMARY]
