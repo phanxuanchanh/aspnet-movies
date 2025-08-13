62b0cd942d8da3c64a246f6ebd5f0134f0bd215c
phanxuanchanh <contact@phanxuanchanh.com>
2025-08-13 10:52:41 +0700

    Rename table names to plural forms

--- Db0Web/Tables/dbo.PeopleLinks.sql
+++ Db0Web/Tables/dbo.PeopleLinks.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[PeopleLink] (
+﻿CREATE TABLE [dbo].[PeopleLinks] (
   [FilmId] [varchar](100) NOT NULL,
   [PersonId] [bigint] NOT NULL,
   PRIMARY KEY CLUSTERED ([FilmId], [PersonId])
@@ -6,10 +6,10 @@
 ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[PeopleLink]
+ALTER TABLE [dbo].[PeopleLinks]
   ADD FOREIGN KEY ([FilmId]) REFERENCES [dbo].[Films] ([ID])
 GO
 
-ALTER TABLE [dbo].[PeopleLink]
+ALTER TABLE [dbo].[PeopleLinks]
   ADD FOREIGN KEY ([PersonId]) REFERENCES [dbo].[People] ([Id])
 GO
